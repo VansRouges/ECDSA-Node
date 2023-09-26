9cfca512e5abe8f0e8f9959bfcd27ea90bd3737f
VansRouges <evansagina57@gmail.com>
2023-09-26 13:16:10 +0100

    updated client to typesript

--- client/src/Wallet.tsx
+++ client/src/Wallet.tsx
@@ -1,9 +1,19 @@
 import server from "./server";
 import * as secp from 'ethereum-cryptography/secp256k1'
 import { toHex } from "ethereum-cryptography/utils"
+import React, { useState } from 'react';
 
-function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
-  async function onChange(evt) {
+interface WalletProps {
+  address: string;
+  setAddress: (address: string) => void;
+  balance: number;
+  setBalance: (balance: number) => void;
+  privateKey: string;
+  setPrivateKey: (privateKey: string) => void;
+}
+
+function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }: WalletProps) {
+  async function onChange(evt: React.ChangeEvent<HTMLInputElement>) {
     const privateKey = evt.target.value;
     setPrivateKey(privateKey);
     const address = toHex(secp.secp256k1.getPublicKey(privateKey))
